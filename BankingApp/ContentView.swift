502f7c283b7d56ad54169fc6008cf4084ff03b0b
karthikravikumar8 <karthikravikumar94@gmail.com>
2023-08-15 21:14:09 +0300

    enabled custom color set and dark theme

--- BankingApp/ContentView.swift
+++ BankingApp/ContentView.swift
@@ -3,13 +3,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            NavigationView {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 24) {
+                        Text("Overview")
+                            .font(.title2)
+                            .bold()
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                }
+                .background(Color.background)
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .navigationViewStyle(.stack)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
